2a0b73a65d45983057d5515a55c7059784d5555b
salinpaul valooran <salinpv@hotmail.com>
2023-11-07 01:01:01 +0530

    function to validate name

--- version_01/index.js
+++ version_01/index.js
@@ -4,3 +4,16 @@ let emailError = document.getElementById("email-error");
 let messageError = document.getElementById("message-error");
 let submitError = document.getElementById("submit-error");
 
+function validateName() {
+  let name = document.getElementById("contact-name").value;
+  if (name.length == 0) {
+    nameError.innerHTML = "Name is required";
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = "First Name and last Name required";
+    return false;
+  }
+  nameError.innerHTML = "Valid name";
+  return true;
+ }
