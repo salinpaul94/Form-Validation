e217e07e958590ee3f96f66c7065a6f9f63b7266
salinpaul valooran <salinpv@hotmail.com>
2023-11-07 01:19:05 +0530

    function for validating phone number

--- version_01/index.js
+++ version_01/index.js
@@ -17,3 +17,21 @@ function validateName() {
   nameError.innerHTML = '<i class="fa-solid fa-check"></i>';
   return true;
  }
+
+ function validatePhone() {
+  let phone = document.getElementById("contact-phone").value;
+  if(phone.length == 0) {
+    phoneError.innerHTML = "Phone No is required";
+    return false;
+  }
+  if(phone.length !== 10){
+    phoneError.innerHTML = "Phone No should be 10 digits";
+    return false;
+  }
+  if(!phone.match(/^[0-9]{10}$/)) {
+    phoneError.innerHTML = "Phone number should be integer";
+    return false;
+  }
+  phoneError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
+ }
