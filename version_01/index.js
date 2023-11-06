d84e6085dacbf46f9ece84a21f5e2d8784d3aad2
salinpaul valooran <salinpv@hotmail.com>
2023-11-07 00:49:50 +0530

    variable for getting span for error message

---
+++ version_01/index.js
@@ -0,0 +1,5 @@
+let nameError = document.getElementById("name-error");
+let phoneError = document.getElementById("phone-error");
+let emailError = document.getElementById("email-error");
+let messageError = document.getElementById("message-error");
+let submitError = document.getElementById("submit-error");
